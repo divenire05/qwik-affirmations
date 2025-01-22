8f598114bbd2125b2106c7f11507b06bfa3db046
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-22 11:46:00 -0500

    metadata

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,21 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const displayIndex = useSignal(0)
   return (
     <div>
-      
+
     </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Affirmations",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "This is a feel good app that reminds you of all the positive things people have to say about you!",
     },
   ],
 };
