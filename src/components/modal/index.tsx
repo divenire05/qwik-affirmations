20ac9034daf94cf0613eea4750662d920235c9c1
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-22 11:35:34 -0500

    creating components

---
+++ src/components/modal/index.tsx
@@ -0,0 +1,19 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <div>
+      
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+};
