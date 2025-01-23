c36ce84ec829dd3cd4edd513cc33e08379509ece
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-23 02:07:58 -0500

    adding useVisibleTask to rotate through affirmations

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,21 @@
-import { component$, useContext, useSignal } from "@builder.io/qwik";
+import { component$, useContext, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { MyContext } from "./layout";
 
 export default component$(() => {
   const displayIndex = useSignal(0)
   const data = useContext(MyContext)
+
+  useVisibleTask$(({cleanup}) => {
+    const interval = setInterval(() => {
+      if (displayIndex.value < data.affirmations.length - 1) {
+        displayIndex.value++
+      } else {
+        displayIndex.value = 0
+      }
+    }, 30000)
+    cleanup(() => clearInterval(interval))
+  })
 
   return (
     <>
